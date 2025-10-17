27bfc63d0956cacf9b13a2067dbb697ad705223b
Laksman Dhinahar <laksmandhinahar@gmail.com>
2025-10-17 23:13:10 +0530

    Update test.py

--- frontend/test.py
+++ frontend/test.py
@@ -1,1 +1,6 @@
+import pytesseract
 
+# Set the path to the tesseract executable if needed
+pytesseract.pytesseract.tesseract_cmd = r"D:\tesseract\tesseract.exe"  # Update this path if necessary
+
+print(pytesseract.get_tesseract_version())
